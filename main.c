05809ee415c1a669d47eb463f1093773b780f3f5
Milanmano <milanmano@gmail.com>
2023-05-10 01:50:18 +0200

    fixing intersection errors + refactor

--- main.c
+++ main.c
@@ -21,6 +21,199 @@ Line g_walls[NUM_OF_WALLS];
 int g_window;
 Node *g_tree;
 
+int getSide(Line splitLine, Point point) {
+    double det = (splitLine.end.x - splitLine.start.x) * (point.y - splitLine.start.y) -
+                 (point.x - splitLine.start.x) * (splitLine.end.y - splitLine.start.y);
+
+    if (det < 0) {
+        return LEFT_SIDE;
+    } else if (det > 0) {
+        return RIGHT_SIDE;
+    } else {
+        return ON_SPLIT_LINE;
+    }
+}
+
+Point calculateIntersection(Point start1, Point end1, Point start2, Point end2) {
+    Point intersection;
+    double x1 = start1.x;
+    double y1 = start1.y;
+    double x2 = end1.x;
+    double y2 = end1.y;
+    double x3 = start2.x;
+    double y3 = start2.y;
+    double x4 = end2.x;
+    double y4 = end2.y;
+
+    double denominator = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+
+    if (denominator == 0) {
+        intersection.x = -1;
+        intersection.y = -1;
+    } else {
+        double numerator1 = (x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4);
+        double numerator2 = (x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3);
+
+        double t1 = numerator1 / denominator;
+        double t2 = numerator2 / denominator;
+
+        if (t1 >= 0 && t1 <= 1 && t2 >= 0 && t2 <= 1) {
+            intersection.x = x1 + t1 * (x2 - x1);
+            intersection.y = y1 + t1 * (y2 - y1);
+        } else if (t1 >= 0 && t1 <= 1) {
+            intersection.x = x1 + t1 * (x2 - x1);
+            intersection.y = y1 + t1 * (y2 - y1);
+        } else {
+            intersection.x = -1;
+            intersection.y = -1;
+        }
+    }
+
+    return intersection;
+}
+
+Node *buildBSP(Line *lines, int numLines) {
+    if (numLines == 0) {
+        return NULL;
+    } else {
+        int randomIndex = rand() % numLines;
+        Line splitLine = lines[randomIndex];
+
+        Line leftLines[100];
+        Line rightLines[100];
+        int numLeftLines = 0;
+        int numRightLines = 0;
+        for (int i = 0; i < numLines; i++) {
+            if (i == randomIndex) {
+                continue;
+            }
+            int sideStart = getSide(splitLine, lines[i].start);
+            int sideEnd = getSide(splitLine, lines[i].end);
+
+            if ((sideStart == LEFT_SIDE && sideEnd == LEFT_SIDE) ||
+                (sideStart == LEFT_SIDE && sideEnd == ON_SPLIT_LINE) ||
+                (sideStart == ON_SPLIT_LINE && sideEnd == LEFT_SIDE)) {
+                leftLines[numLeftLines++] = lines[i];
+            } else if ((sideStart == RIGHT_SIDE && sideEnd == RIGHT_SIDE) ||
+                       (sideStart == RIGHT_SIDE && sideEnd == ON_SPLIT_LINE) ||
+                       (sideStart == ON_SPLIT_LINE && sideEnd == RIGHT_SIDE)) {
+                rightLines[numRightLines++] = lines[i];
+            } else if ((sideStart == RIGHT_SIDE && sideEnd == LEFT_SIDE) ||
+                       (sideStart == LEFT_SIDE && sideEnd == RIGHT_SIDE)) {
+                Point intersection = calculateIntersection(lines[i].start,
+                                                           lines[i].end, splitLine.start, splitLine.end);
+
+                Line leftPart = {lines[i].start, intersection};
+                Line rightPart = {intersection, lines[i].end};
+                leftPart.color[0] = lines[i].color[0];
+                leftPart.color[1] = lines[i].color[1];
+                leftPart.color[2] = lines[i].color[2];
+                rightPart.color[0] = lines[i].color[0];
+                rightPart.color[1] = lines[i].color[1];
+                rightPart.color[2] = lines[i].color[2];
+                leftLines[numLeftLines++] = leftPart;
+                rightLines[numRightLines++] = rightPart;
+            } else {
+                if (rand() % 2 == 0) {
+                    leftLines[numLeftLines++] = lines[i];
+                } else {
+                    rightLines[numRightLines++] = lines[i];
+                }
+            }
+        }
+
+        Node *leftSubtree = buildBSP(leftLines, numLeftLines);
+        Node *rightSubtree = buildBSP(rightLines, numRightLines);
+
+        Node *newNode = createNode(splitLine);
+        newNode->left = leftSubtree;
+        newNode->right = rightSubtree;
+        return newNode;
+    }
+}
+
+void storeTree(struct Node *root, FILE *file) {
+    if (root == NULL) {
+        fwrite("NULL", sizeof(char), 4, file);
+        return;
+    }
+
+    fwrite(&(root->line.start.x), sizeof(double), 1, file);
+    fwrite(&(root->line.start.y), sizeof(double), 1, file);
+    fwrite(&(root->line.end.x), sizeof(double), 1, file);
+    fwrite(&(root->line.end.y), sizeof(double), 1, file);
+    fwrite(&(root->line.color[0]), sizeof(int), 1, file);
+    fwrite(&(root->line.color[1]), sizeof(int), 1, file);
+    fwrite(&(root->line.color[2]), sizeof(int), 1, file);
+
+    storeTree(root->left, file);
+    storeTree(root->right, file);
+}
+
+void printTree(struct Node *node) {
+    if (node != NULL) {
+        printTree(node->left);
+        printf("(%.0f,%.0f) -> (%.0f,%.0f)\n", node->line.start.x, node->line.start.y, node->line.end.x,
+               node->line.end.y);
+        printTree(node->right);
+    }
+}
+
+void drawLine(vec2 *start, vec2 *end, vec3 *color) {
+
+    line *l = (line *) malloc(sizeof(line));
+
+    float x1 = start->x;
+    float y1 = start->y;
+    float x2 = end->x;
+    float y2 = end->y;
+    float w = 320;
+    float h = 240;
+
+    x1 = 2 * x1 / w - 1;
+    y1 = 2 * y1 / h - 1;
+
+    x2 = 2 * x2 / w - 1;
+    y2 = 2 * y2 / h - 1;
+
+    start->x = x1;
+    start->y = y1;
+    end->x = x2;
+    end->y = y2;
+
+    l->lineColor = color;
+
+    l->vertices = (float *) (malloc(sizeof(float) * 6));
+    l->vertices[0] = start->x;
+    l->vertices[1] = start->y;
+    l->vertices[2] = end->x;
+    l->vertices[3] = end->y;
+
+    glGenVertexArrays(1, &l->VAO);
+    glGenBuffers(1, &l->VBO);
+    glBindVertexArray(l->VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, l->VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 4,
+                 &(l->vertices[0]),
+                 GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE,
+                          2 * sizeof(float), (void *) 0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    GLint shaderProgram;
+    glGetIntegerv(GL_CURRENT_PROGRAM, &shaderProgram);
+
+    GLint colorloc = glGetUniformLocation(shaderProgram, "color");
+    glUniform3fv(colorloc, 1, &(l->lineColor->x));
+    glBindVertexArray(l->VAO);
+    glDrawArrays(GL_LINES, 0, 2);
+}
+
 void drawWall(int x1, int b1, int t1, int x2, int b2, int t2, int color[3]) {
     double dx = x2 - x1;
     if (dx == 0) {
@@ -58,7 +251,7 @@ void drawWall(int x1, int b1, int t1, int x2, int b2, int t2, int color[3]) {
             tys = GLUT_WINDOW_SIZE_HEIGHT;
         }
 
-        draw_line(vec2_init(x, bys), vec2_init(x, tys), vec3_init(color[0], color[1], color[2]));
+        drawLine(vec2_init(x, bys), vec2_init(x, tys), vec3_init(color[0], color[1], color[2]));
     }
 }
 
@@ -78,7 +271,7 @@ void clipBehindPlayer(int *x1, int *y1, int *z1, int x2, int y2, int z2) {
     *z1 = *z1 + s * (z2 - (*z1));
 }
 
-void draw_3D_Wall(double start_x, double start_y, double end_x, double end_y, int color[3]) {
+void draw3DWall(double start_x, double start_y, double end_x, double end_y, int color[3]) {
     int wx[4], wy[4], wz[4];
     int x1 = start_x - g_player.x;
     int y1 = start_y - g_player.y;
@@ -129,7 +322,6 @@ void draw_3D_Wall(double start_x, double start_y, double end_x, double end_y, in
     drawWall(wx[0], wy[0], wy[2], wx[1], wy[1], wy[3], color);
 };
 
-
 void drawTree(struct Node *node) {
     if (node == NULL) { return; }
     Point p = {g_player.x, g_player.y};
@@ -137,14 +329,14 @@ void drawTree(struct Node *node) {
 
     if (side == LEFT_SIDE) {
         drawTree(node->right);
-        draw_3D_Wall(node->line.start.x, node->line.start.y, node->line.end.x, node->line.end.y, node->line.color);
+        draw3DWall(node->line.start.x, node->line.start.y, node->line.end.x, node->line.end.y, node->line.color);
         drawTree(node->left);
     } else if (side == RIGHT_SIDE) {
         drawTree(node->left);
-        draw_3D_Wall(node->line.start.x, node->line.start.y, node->line.end.x, node->line.end.y, node->line.color);
+        draw3DWall(node->line.start.x, node->line.start.y, node->line.end.x, node->line.end.y, node->line.color);
         drawTree(node->right);
     } else if (node->left == NULL && node->right == NULL) {
-        draw_3D_Wall(node->line.start.x, node->line.start.y, node->line.end.x, node->line.end.y, node->line.color);
+        draw3DWall(node->line.start.x, node->line.start.y, node->line.end.x, node->line.end.y, node->line.color);
     }
 
 }
@@ -320,7 +512,7 @@ void fileRead(const char arg[]) {
         fclose(fp);
     }
 
-    print_tree(g_tree);
+    printTree(g_tree);
 
 }
 
@@ -328,8 +520,8 @@ void initialization(char arg[]) {
     timer();
     fileRead(arg);
 
-    g_player.x = 80;
-    g_player.y = 10;
+    g_player.x = 30;
+    g_player.y = 20;
     g_player.rotation = 0;
 
     g_pressedKeys.w = 0;
--- structs.h
+++ structs.h
@@ -25,148 +25,6 @@ typedef struct {
     int color[3];
 } Line;
 
-typedef struct Node {
-    Line line;
-    struct Node *left;
-    struct Node *right;
-} Node;
-
-Node *createNode(Line line) {
-    Node *newNode = (Node *) malloc(sizeof(Node));
-    newNode->line = line;
-    newNode->left = NULL;
-    newNode->right = NULL;
-    return newNode;
-}
-
-int getSide(Line splitLine, Point point) {
-    double det = (splitLine.end.x - splitLine.start.x) * (point.y - splitLine.start.y) -
-                 (point.x - splitLine.start.x) * (splitLine.end.y - splitLine.start.y);
-
-    if (det < 0) {
-        return LEFT_SIDE;
-    } else if (det > 0) {
-        return RIGHT_SIDE;
-    } else {
-        return ON_SPLIT_LINE;
-    }
-}
-
-Node *buildBSP(Line *lines, int numLines) {
-    if (numLines == 0) {
-        return NULL;
-    } else {
-        int randomIndex = rand() % numLines;
-        Line splitLine = lines[randomIndex];
-
-        Line leftLines[100];
-        Line rightLines[100];
-        int numLeftLines = 0;
-        int numRightLines = 0;
-        for (int i = 0; i < numLines; i++) {
-            if (i == randomIndex) {
-                continue;
-            }
-            int sideStart = getSide(splitLine, lines[i].start);
-            int sideEnd = getSide(splitLine, lines[i].end);
-
-            if ((sideStart == LEFT_SIDE && sideEnd == LEFT_SIDE) ||
-                (sideStart == LEFT_SIDE && sideEnd == ON_SPLIT_LINE) ||
-                (sideStart == ON_SPLIT_LINE && sideEnd == LEFT_SIDE)) {
-                leftLines[numLeftLines++] = lines[i];
-            } else if ((sideStart == RIGHT_SIDE && sideEnd == RIGHT_SIDE) ||
-                       (sideStart == RIGHT_SIDE && sideEnd == ON_SPLIT_LINE) ||
-                       (sideStart == ON_SPLIT_LINE && sideEnd == RIGHT_SIDE)) {
-                rightLines[numRightLines++] = lines[i];
-            } else if ((sideStart == RIGHT_SIDE && sideEnd == LEFT_SIDE) ||
-                       (sideStart == LEFT_SIDE && sideEnd == RIGHT_SIDE)) {
-                Point intersection;
-                intersection.x = (lines[i].start.x +
-                                  (lines[i].end.x - lines[i].start.x) * (splitLine.start.y - lines[i].start.y) /
-                                  (lines[i].end.y - lines[i].start.y));
-                intersection.y = splitLine.start.y;
-                Line leftPart = {lines[i].start, intersection};
-                Line rightPart = {intersection, lines[i].end};
-                leftPart.color[0] = lines[i].color[0];
-                leftPart.color[1] = lines[i].color[1];
-                leftPart.color[2] = lines[i].color[2];
-                rightPart.color[0] = lines[i].color[0];
-                rightPart.color[1] = lines[i].color[1];
-                rightPart.color[2] = lines[i].color[2];
-                leftLines[numLeftLines++] = leftPart;
-                rightLines[numRightLines++] = rightPart;
-            } else {
-                if (rand() % 2 == 0) {
-                    leftLines[numLeftLines++] = lines[i];
-                } else {
-                    rightLines[numRightLines++] = lines[i];
-                }
-            }
-        }
-
-        Node *leftSubtree = buildBSP(leftLines, numLeftLines);
-        Node *rightSubtree = buildBSP(rightLines, numRightLines);
-
-        Node *newNode = createNode(splitLine);
-        newNode->left = leftSubtree;
-        newNode->right = rightSubtree;
-        return newNode;
-    }
-}
-
-struct Node *readTree(FILE *file) {
-    char marker[4];
-    fread(marker, sizeof(char), 4, file);
-    if (strncmp(marker, "NULL", 4) == 0) {
-        return NULL;
-    } else {
-        fseek(file, -4, SEEK_CUR);
-    }
-
-    Line line;
-    fread(&line.start.x, sizeof(double), 1, file);
-    fread(&line.start.y, sizeof(double), 1, file);
-    fread(&line.end.x, sizeof(double), 1, file);
-    fread(&line.end.y, sizeof(double), 1, file);
-    fread(&line.color[0], sizeof(int), 1, file);
-    fread(&line.color[1], sizeof(int), 1, file);
-    fread(&line.color[2], sizeof(int), 1, file);
-
-    struct Node *newNode = createNode(line);
-
-    newNode->left = readTree(file);
-    newNode->right = readTree(file);
-
-    return newNode;
-}
-
-void storeTree(struct Node *root, FILE *file) {
-    if (root == NULL) {
-        fwrite("NULL", sizeof(char), 4, file);
-        return;
-    }
-
-    fwrite(&(root->line.start.x), sizeof(double), 1, file);
-    fwrite(&(root->line.start.y), sizeof(double), 1, file);
-    fwrite(&(root->line.end.x), sizeof(double), 1, file);
-    fwrite(&(root->line.end.y), sizeof(double), 1, file);
-    fwrite(&(root->line.color[0]), sizeof(int), 1, file);
-    fwrite(&(root->line.color[1]), sizeof(int), 1, file);
-    fwrite(&(root->line.color[2]), sizeof(int), 1, file);
-
-    storeTree(root->left, file);
-    storeTree(root->right, file);
-}
-
-void print_tree(struct Node *node) {
-    if (node != NULL) {
-        print_tree(node->left);
-        printf("(%.0f,%.0f) -> (%.0f,%.0f)\n", node->line.start.x, node->line.start.y, node->line.end.x,
-               node->line.end.y);
-        print_tree(node->right);
-    }
-}
-
 typedef struct vec2 {
     float x;
     float y;
@@ -196,67 +54,47 @@ vec3 *vec3_init(float _x, float _y, float _z) {
 typedef struct line {
     unsigned int VBO, VAO;
     float *vertices;
-    vec2 *startPoint;
-    vec2 *endPoint;
     vec3 *lineColor;
 } line;
 
+typedef struct Node {
+    Line line;
+    struct Node *left;
+    struct Node *right;
+} Node;
 
-void draw_line(vec2 *start, vec2 *end, vec3 *color) {
+Node *createNode(Line line) {
+    Node *newNode = (Node *) malloc(sizeof(Node));
+    newNode->line = line;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
 
-    line *l = (line *) malloc(sizeof(line));
+struct Node *readTree(FILE *file) {
+    char marker[4];
+    fread(marker, sizeof(char), 4, file);
+    if (strncmp(marker, "NULL", 4) == 0) {
+        return NULL;
+    } else {
+        fseek(file, -4, SEEK_CUR);
+    }
 
-    float x1 = start->x;
-    float y1 = start->y;
-    float x2 = end->x;
-    float y2 = end->y;
-    float w = 320;
-    float h = 240;
+    Line line;
+    fread(&line.start.x, sizeof(double), 1, file);
+    fread(&line.start.y, sizeof(double), 1, file);
+    fread(&line.end.x, sizeof(double), 1, file);
+    fread(&line.end.y, sizeof(double), 1, file);
+    fread(&line.color[0], sizeof(int), 1, file);
+    fread(&line.color[1], sizeof(int), 1, file);
+    fread(&line.color[2], sizeof(int), 1, file);
 
-    x1 = 2 * x1 / w - 1;
-    y1 = 2 * y1 / h - 1;
+    struct Node *newNode = createNode(line);
 
-    x2 = 2 * x2 / w - 1;
-    y2 = 2 * y2 / h - 1;
+    newNode->left = readTree(file);
+    newNode->right = readTree(file);
 
-    start->x = x1;
-    start->y = y1;
-    end->x = x2;
-    end->y = y2;
-
-    l->startPoint = start;
-    l->endPoint = end;
-    l->lineColor = color;
-
-    l->vertices = (float *) (malloc(sizeof(float) * 6));
-    l->vertices[0] = start->x;
-    l->vertices[1] = start->y;
-    l->vertices[2] = end->x;
-    l->vertices[3] = end->y;
-
-    glGenVertexArrays(1, &l->VAO);
-    glGenBuffers(1, &l->VBO);
-    glBindVertexArray(l->VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, l->VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 4,
-                 &(l->vertices[0]),
-                 GL_STATIC_DRAW);
-
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE,
-                          2 * sizeof(float), (void *) 0);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
-
-    GLint shaderProgram;
-    glGetIntegerv(GL_CURRENT_PROGRAM, &shaderProgram);
-
-    GLint colorloc = glGetUniformLocation(shaderProgram, "color");
-    glUniform3fv(colorloc, 1, &(l->lineColor->x));
-    glBindVertexArray(l->VAO);
-    glDrawArrays(GL_LINES, 0, 2);
+    return newNode;
 }
 
 #endif //GOOM_STRUCTS_H
